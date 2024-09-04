c822699e70bc600734286ff75b0486b0b5a2394d
koeurn <pheakaddacc16@gmail.com>
2024-09-04 13:58:51 +0700

    no message

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +20,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/tickets', function (){
-   return \App\Models\Ticket::all();
+   return Ticket::all();
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
