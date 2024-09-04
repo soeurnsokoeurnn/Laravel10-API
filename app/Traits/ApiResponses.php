81699d85eadc01ffdd535ae52f7416324e9de78c
koeurn <pheakaddacc16@gmail.com>
2024-09-04 11:17:29 +0700

    first commit

---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+trait ApiResponses
+{
+    protected function responseSuccess($message): JsonResponse
+    {
+        return $this->successResponse($message, 200);
+    }
+    protected function successResponse($data, $statusCode = 200): JsonResponse
+    {
+        return response()->json([
+            'message' => $data,
+            'statusCode' => $statusCode,
+        ], $statusCode);
+    }
+
+}
